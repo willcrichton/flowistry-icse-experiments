2add1ab9326b1de074fc3226fa2e47eaa7c14b9a
Will Crichton <wcrichto@cs.stanford.edu>
2021-10-07 12:18:10 -0700

    Bump

--- init.sh
+++ init.sh
@@ -22,14 +22,14 @@ function co() {
 
 co https://github.com/SergioBenitez/Rocket Rocket 8d4d01106e2e10b08100805d40bfa19a7357e900 &&
 co https://github.com/hyperium/hyper hyper ed2fdb7b6a2963cea7577df05ddc41c56fee7246 &&
-co https://github.com/image-rs/image image 6e0cd31a5287dd589d2e78ae33c1f720c77a6863 &&
+co https://github.com/image-rs/image image e916e9dda5f4253f6cc4557b0fe5fa3876ac18e5 &&
 co https://github.com/dimforge/nalgebra nalgebra 984bb1a63943aa68b6f26ff4a6acf8f68b833b70 &&
 co https://github.com/xiph/rav1e rav1e 1b6643324752785e7cd6ad0b19257f3c3a9b2c6a &&
 co https://github.com/rayon-rs/rayon/ rayon c571f8ffb4f74c8c09b4e1e6d9979b71b4414d07 &&
 co https://github.com/mrDIMAS/rg3d rg3d ca7b85f2b30e45b82caee0591ee1abf65bb3eb00 &&
 co https://github.com/ctz/rustls rustls cdf1dada21a537e141d0c6dde9c5685bb43fbc0e &&
 co https://github.com/mozilla/sccache sccache 3f318a8675e4c3de4f5e8ab2d086189f2ae5f5cf &&
-co https://github.com/RustPython/RustPython RustPython c5d1f4afaaddc0b79b8be4cc923bf2e63791eece
+co https://github.com/RustPython/RustPython RustPython 9143e51b7524a5084d5ed230b1f2f5b0610ac58b
 popd
 
 # Install native script dependencies
--- notebooks/rs_utils.rs
+++ notebooks/rs_utils.rs
@@ -1,43 +1,48 @@
 // serde_json = "1.0"
 // serde = {version = "1.0", features = ["derive"]}
-// pythonize = "0.13"
+// pythonize = "0.14"
 // rayon = "1.5"
+// itertools = "0.10"
+#![feature(rustc_private)]
 
+extern crate rustc_macros;
+extern crate rustc_serialize;
+
+use itertools::Itertools;
 use pyo3::prelude::*;
 use pyo3::{exceptions::PyException, wrap_pyfunction};
 use rayon::prelude::*;
-use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::BufReader;
+use rustc_macros::Decodable;
+use rustc_serialize::json;
+use serde::Serialize;
+use std::fs;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Decodable, Clone)]
 pub struct Range {
-  pub start_line: usize,
-  pub start_col: usize,
-  pub end_line: usize,
-  pub end_col: usize,
+  pub start: usize,
+  pub end: usize,
   pub filename: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Serialize, Decodable, Clone, PartialEq, Eq)]
 pub enum MutabilityMode {
   DistinguishMut,
   IgnoreMut,
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Serialize, Decodable, Clone, PartialEq, Eq)]
 pub enum ContextMode {
   SigOnly,
   Recurse,
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Serialize, Decodable, Clone, PartialEq, Eq)]
 pub enum PointerMode {
   Precise,
   Conservative,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Decodable, Clone)]
 pub struct EvalResult {
   mutability_mode: MutabilityMode,
   context_mode: ContextMode,
@@ -47,8 +52,6 @@ pub struct EvalResult {
   function_path: String,
   num_instructions: usize,
   num_relevant_instructions: usize,
-  num_tokens: usize,
-  num_relevant_tokens: usize,
   duration: f64,
   has_immut_ptr_in_call: bool,
   has_same_type_ptrs_in_call: bool,
@@ -64,12 +67,15 @@ pub struct EvalResult {
 
 #[pyfunction]
 fn parse_data(py: Python, path: String) -> PyResult<PyObject> {
-  let file = File::open(path)?;
-  let reader = BufReader::new(file);
-  let mut data: Vec<Vec<EvalResult>> =
-    serde_json::from_reader(reader).map_err(|err| PyException::new_err(format!("{}", err)))?;
+  let contents = String::from_utf8(fs::read(&path)?)?;
+  let data: Vec<EvalResult> = json::decode(&contents).map_err(|err| PyException::new_err(format!("{}", err)))?;
+  let mut trials = data
+    .into_iter()
+    .into_group_map_by(|sample| (sample.function_path.clone(), sample.sliced_local))
+    .into_values()
+    .collect::<Vec<_>>();
 
-  let updated_data = data
+  let updated_data = trials
     .par_iter_mut()
     .map(|trial| {
       let min_sample = trial
@@ -89,15 +95,16 @@ fn parse_data(py: Python, path: String) -> PyResult<PyObject> {
       trial
         .into_iter()
         .map(|mut sample| {
+          let frac = |a, b| (a - b) as f64 / (if b == 0 { 1.0 } else { b as f64 });
           let min_inst = min_sample.num_relevant_instructions;
           sample.instructions_relative = Some(sample.num_relevant_instructions - min_inst);
           sample.instructions_relative_frac =
-            Some((sample.num_relevant_instructions - min_inst) as f64 / (min_inst as f64));
+            Some(frac(sample.num_relevant_instructions, min_inst));
           sample.reached_library = min_sample.reached_library;
           let base_inst = base_sample.num_relevant_instructions;
           sample.instructions_relative_base = Some(sample.num_relevant_instructions - base_inst);
           sample.instructions_relative_base_frac =
-            Some((sample.num_relevant_instructions - base_inst) as f64 / (base_inst as f64));
+            Some(frac(sample.num_relevant_instructions, base_inst));
           sample.baseline_reached_library = Some(min_sample.reached_library);
           sample
         })
